6b6ccc466c2ec4a279ed65ecc2901a5979cb65d7
Diogo-AB <117372616+Diogo-AB@users.noreply.github.com>
2023-06-20 17:29:21 -0300

    Update Arvore-Genealogica.c

--- Arvore-Genealogica.c
+++ Arvore-Genealogica.c
@@ -32,7 +32,7 @@ Metodo de pesquisa em busca de informacoes do projeto: Bing AI */
 
 
 //estrutura das doencas que essa pessoa pode ter.
-typedef struct Ficha_Tecnica{                                                      
+typedef struct Ficha_Tecnica{
 
     int saudavel;                                       //usuario saudavel(nao possui nenhuma doenca)
     //doencas hereditarias
@@ -42,13 +42,13 @@ typedef struct Ficha_Tecnica{
     //doencas nao hereditarias.
     int Diabetes;                                       //Diabetes Tipo 2
     int Doenca_Cardiovascular;                          //Doenca Cardiovascular
-                                                
+
 }ficha;                                                                                            //tabela de IDs:        doencas: 1000 - 1005
 ficha doencas = {1000, 1001, 1002, 1003, 1004, 1005};   //iniciando a estrutura com valores(IDs).
 
-//estrutura com as informacoes de cada um dos 2.                                                    pessoas: formado por 10 digitos(exemplo: 000.000.000.0):   
+//estrutura com as informacoes de cada um dos 2.                                                    pessoas: formado por 10 digitos(exemplo: 000.000.000.0):
 typedef struct FILHO{                                                                               // 1 digito(sexo): 1 = homem / 0 = mulher.
-                                                                                                 
+
     //informacoes basicas.                                                                          // 2 digito(estado): 1 = casado / 0 = solteiro.
     char ID[8];
     char nome[50];                                                                                  // 3 digito(idade): 0 = idade < 18 / 1 = 18 > idade < 80 / 2 = idade > 80.
@@ -58,7 +58,7 @@ typedef struct FILHO{                                                           
     //informacoes sobre suas doencas                                                                // 10 digito(nivel): definine onde que o casal esta na arvore.
 
     struct Doenca *doenca[1];               //array de ponteiros para apenas 1, ou seja cada pessoa so vai poder ter uma ficha tecnica.
-    
+
 }filho;
 
 typedef struct CONJUGUE{
@@ -75,17 +75,17 @@ typedef struct Casal{
     char ID[8];                            //ID do casal vai ter 7 digitos(3 de cada pessoa, 1 de descendencia(ex: 000.000.0)).
     struct FILHO *filho;                   //descendecia: filho pertencente a arvore(1), conjugue(0). //(idade,descedencia(pai),nivel).
     struct CONJUGUE *conjugue;
-     struct Casal* herdeiro;
+    struct Casal* herdeiro;
 
 }casal;                                    //*pessoa[2] (estrutura casal recebe 2 pessoas(struct pessoa)).
 
 //estrutura da arvore em grafos.
 
 typedef struct noAdjacente {
-    struct no* vertice; 
+    struct no* vertice;
     struct noAdjacente* prox;
 } NoAdjacente;
- 
+
 typedef struct ListaNoAdjacente{
 
     int tam;
@@ -119,7 +119,7 @@ typedef struct {
 short int   cria_pastas         (void);                     //funcao para a criacao de pastas, tipo short int pois o short int ja é mais que o necessario para essa funcao.
 int         escrever_arquivo    (casal *C, char opcao);     //funcao para escrever as informacoes em um arquivo binario.
 short int   verificaPasta       (const char* nomePasta);
-short int   existe_pessoa       (const char *nome_arquivo_registro);     
+short int   existe_pessoa       (const char *nome_arquivo_registro);
 //void        ler_arquivo         (const char* nomePasta, char nome_arquivo_registro);            //funcao para ler os dados da pessoa.
 
 //funcoes de manipulacao do grafo.
@@ -162,7 +162,7 @@ int main(void){
     Adiciona_Pessoa(&C);
     char ID_CASAL[8] = "101101";
     adicionarNo(arvore, ID_CASAL,&C);
-    
+
     printf("-----|ARVORE GENEALOGICA.|-----\n");
     salvarGrafo(arvore, "arvore");
 
@@ -186,7 +186,7 @@ short int cria_pastas(void){
         printf("A pasta 'Mulheres' ja existe.\n");
     }
 
-    if(!verificaPasta("./Homens")){    
+    if(!verificaPasta("./Homens")){
 
         status = mkdir("./Homens");                 //cria a pasta reservada aos arquivos dos homens.
             if(status == 0){
@@ -234,10 +234,10 @@ short int Adiciona_Pessoa(casal *C){
     do{
         printf("Quem voce deseja adicionar: filho(1) / conjugue(2)?\n");
         fgets(opcao, sizeof(opcao), stdin);
-        
+
         getchar();
         printf("opcao: %s\n",opcao);
-        
+
         if(opcao[0] == '1'){
             Adiciona_filho(C);
             Adiciona_par(C);
@@ -276,24 +276,24 @@ short int Adiciona_Pessoa(casal *C){
     return 1;
 }
 
-void Adiciona_filho(casal *C){    
+void Adiciona_filho(casal *C){
 
     C->filho = malloc(sizeof(filho)); // Aloca memória para a estrutura filho dentro de casal
     if (C->filho == NULL) {
         // Trate o erro de alocação de memória
         printf("Erro ao alocar memória para filho\n");
         return;
-    }        
+    }
 
     printf("Digite o ID do filho: ");                       //usando fgets para ter mais segurança e nao ocasionar em um overflow, fgets normalmente le uma linha inteira de uma só vez.
     fgets(C->filho->ID, sizeof(C->filho->ID), stdin);       //fgets precisa ter 3 definicoes (ponteiro onde vai armazenar a string lida, tamanho, ponteiro para o fluxo de entrada, normalmente se usa stdin).
     C->filho->ID[strcspn(C->filho->ID, "\n")] = '\0';       //substitui o caractere de quebra de linha (\n) pelo caractere nulo (\0) na string filho.nome.  //evita possiveis problemas na string
     printf("Digite o nome: ");
     fgets(C->filho->nome, sizeof(C->filho->nome), stdin);
-    C->filho->nome[strcspn(C->filho->nome, "\n")] = '\0'; 
+    C->filho->nome[strcspn(C->filho->nome, "\n")] = '\0';
 
     printf("Digite a idade: ");
-    char input[4];                          //por idade ser um inteiro, não vou ter uma string de comparacao, então criei uma variavel para armazenar. 
+    char input[4];                          //por idade ser um inteiro, não vou ter uma string de comparacao, então criei uma variavel para armazenar.
     fgets(input, sizeof(input), stdin);     //char input[4] pois tendo em vista que idade nunca vai ser maior que 3 digitos.        //fgets usando a mesma logica que o de cima.
     C->filho->idade = (int) strtol(input, NULL, 10);// armazenando em filho.idade  //strol(função que transforma string em int), 3 campos(ponteiro para a string, NULL pois vai ser a string inteira, base numerica)
 
@@ -311,7 +311,7 @@ void Adiciona_conjugue(casal *C){
         // Trate o erro de alocação de memória
         printf("Erro ao alocar memória para conjugue\n");
         return;
-    }  
+    }
 
     printf("Digite o ID: ");                //usando fgets para ter mais segurança e nao ocasionar em um overflow, fgets normalmente le uma linha inteira de uma só vez.
     fgets(C->conjugue->ID, sizeof((C->conjugue->ID)), stdin);   //fgets precisa ter 3 definicoes (ponteiro onde vai armazenar a string lida, tamanho, ponteiro para o fluxo de entrada, normalmente se usa stdin).
@@ -319,10 +319,10 @@ void Adiciona_conjugue(casal *C){
 
     printf("Digite o nome: ");
     fgets(C->conjugue->nome, sizeof(C->conjugue->nome), stdin);
-    C->conjugue->nome[strcspn(C->conjugue->nome, "\n")] = '\0'; 
+    C->conjugue->nome[strcspn(C->conjugue->nome, "\n")] = '\0';
 
     printf("Digite a idade: ");
-    char input[4];                          //por idade ser um inteiro, não vou ter uma string de comparacao, então criei uma variavel para armazenar. 
+    char input[4];                          //por idade ser um inteiro, não vou ter uma string de comparacao, então criei uma variavel para armazenar.
     fgets(input, sizeof(input), stdin);     //char input[4] pois tendo em vista que idade nunca vai ser maior que 3 digitos.        //fgets usando a mesma logica que o de cima.
     C->conjugue->idade = (int) strtol(input, NULL, 10);// armazenando em conjugue.idade  //strol(função que transforma string em int), 3 campos(ponteiro para a string, NULL pois vai ser a string inteira, base numerica)
 
@@ -332,7 +332,36 @@ void Adiciona_conjugue(casal *C){
     C->conjugue->peso = strtof(input_float, NULL);    //strtof para transformar em float                      //essa função não recebe o argumento de base numerica.
 
 }
+/*
+void Adiciona_ficha_tecnica(ficha *C){
 
+    C->ficha = malloc(sizeof(ficha));
+    if (C->ficha == NULL) {
+        printf("Erro ao alocar memória para a ficha tecnica\n");
+        return;
+    }
+
+    printf("Digite (1) se for saudavel ou (2) se tiver alguma doenca: ");
+    if(
+    fgets(C->conjugue->ID, sizeof((C->conjugue->ID)), stdin);   //fgets precisa ter 3 definicoes (ponteiro onde vai armazenar a string lida, tamanho, ponteiro para o fluxo de entrada, normalmente se usa stdin).
+    C->conjugue->ID[strcspn(C->conjugue->ID, "\n")] = '\0';     //substitui o caractere de quebra de linha (\n) pelo caractere nulo (\0) na string conjugue.nome.  //evita possiveis problemas na string
+
+    printf("Digite o nome: ");
+    fgets(C->conjugue->nome, sizeof(C->conjugue->nome), stdin);
+    C->conjugue->nome[strcspn(C->conjugue->nome, "\n")] = '\0';
+
+    printf("Digite a idade: ");
+    char input[4];                          //por idade ser um inteiro, não vou ter uma string de comparacao, então criei uma variavel para armazenar.
+    fgets(input, sizeof(input), stdin);     //char input[4] pois tendo em vista que idade nunca vai ser maior que 3 digitos.        //fgets usando a mesma logica que o de cima.
+    C->conjugue->idade = (int) strtol(input, NULL, 10);// armazenando em conjugue.idade  //strol(função que transforma string em int), 3 campos(ponteiro para a string, NULL pois vai ser a string inteira, base numerica)
+
+    printf("Digite o peso: ");
+    char input_float[6];                    //variavel para armazenar a string que sera lida.
+    fgets(input_float, sizeof(input_float), stdin);
+    C->conjugue->peso = strtof(input_float, NULL);    //strtof para transformar em float                      //essa função não recebe o argumento de base numerica.
+
+}
+*/
 void Adiciona_par(casal *C){
 
     char opcao[2];
@@ -365,7 +394,7 @@ int escrever_arquivo(casal *C, char opcao){
     if(opcao == '1'){
 
         // Salvar em arquivo na pasta "Registro"
-            
+
         sprintf(nome_arquivo_registro, "./Registro/%s.bin", C->filho->ID);
 
         if(!existe_pessoa(nome_arquivo_registro)){
@@ -374,11 +403,11 @@ int escrever_arquivo(casal *C, char opcao){
 
             fgets(op, sizeof(op), stdin);
             if(op[0] == 's' || op[0] == 'S')
-               goto continuar; 
+               goto continuar;
 
             else
                 return 0;
-            
+
         }
         else
             printf("A pessoa nao esta registrada.\n");
@@ -428,7 +457,7 @@ int escrever_arquivo(casal *C, char opcao){
     else if(opcao == '2'){
 
         // Salvar em arquivo na pasta "Registro"
-            
+
         sprintf(nome_arquivo_registro, "./Registro/%s.bin", C->conjugue->ID);
 
         if(!existe_pessoa(nome_arquivo_registro)){
@@ -466,7 +495,7 @@ int escrever_arquivo(casal *C, char opcao){
 
         }
         else if(C->conjugue->ID[0] == '0'){
-    
+
             // Salvar em arquivo na pasta "Mulheres"
             sprintf(nome_arquivo, "./Mulheres/%s.bin", C->conjugue->ID);
 
@@ -478,7 +507,7 @@ int escrever_arquivo(casal *C, char opcao){
             }
             fwrite(&C->conjugue, sizeof(C->conjugue), 1, file);
             fclose(file);
-        }  
+        }
     }
     return 1;
 }
@@ -517,10 +546,10 @@ short int   existe_pessoa       (const char *nome_arquivo_registro){
 //void ler_arquivo(const char* nomePasta, char nome_arquivo_registro){}
 
 Grafo* criarGrafo() {
-                                                    
+
     Grafo* grafo = (Grafo*)malloc(sizeof(Grafo));       // Aloca memória para a struct Grafo
 
-    if (grafo == NULL) {                                // Tratamento de erro, falha na alocação de memória                                
+    if (grafo == NULL) {                                // Tratamento de erro, falha na alocação de memória
         return NULL;
     }
     // Inicializa os campos do grafo
@@ -539,9 +568,9 @@ short int adicionarNo(Grafo* grafo, char ID_CASAL[8], casal *C){
     No* novoNo = (No*)malloc(sizeof(No));
     if (novoNo == NULL) {
         printf("Falha ao criar o nó.\n");
-        return 1;  
+        return 1;
     }
-    
+
     // Preenche os campos do novo nó
     strcpy(novoNo->ID_CASAL, ID_CASAL);
     novoNo->casal = C;
@@ -549,7 +578,7 @@ short int adicionarNo(Grafo* grafo, char ID_CASAL[8], casal *C){
     novoNo->nosAdjacentes.inicio = NULL;
     novoNo->nosAdjacentes.fim = NULL;
     novoNo->prox = NULL;
-    
+
     // Verifica se o grafo está vazio
     if (grafo->inicio == NULL) {
         grafo->inicio = novoNo;
@@ -558,7 +587,7 @@ short int adicionarNo(Grafo* grafo, char ID_CASAL[8], casal *C){
         grafo->fim->prox = novoNo;
         grafo->fim = novoNo;
     }
-    
+
     // Incrementa o tamanho do grafo
     grafo->tam++;
 
@@ -586,28 +615,28 @@ void criarConexao(Grafo* grafo, char ID_CASAL1[8], char ID_CASAL2[8]) {
 }
 
 void salvarGrafo(Grafo* grafo, const char* nomeArquivo) {
-    
+
     // Criar o caminho completo para o arquivo
     char caminhoCompleto[10]; // Tamanho arbitrário, ajuste conforme necessário
     strcpy(caminhoCompleto, "Grafo/"); // Substitua "Grafo" pelo nome da pasta desejada
     strcat(caminhoCompleto, nomeArquivo);
-    
+
     // Abrir o arquivo para escrita em modo binário
     FILE* arquivo = fopen(caminhoCompleto, "wb");
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo para escrita.\n");
         return;
     }
-    
+
     // Criar uma estrutura de dados para salvar o grafo
     Grafo grafoSalvo;
     grafoSalvo.tam = grafo->tam;
     grafoSalvo.inicio = grafo->inicio;
     grafoSalvo.fim = grafo->fim;
-    
+
     // Escrever a estrutura no arquivo
     fwrite(&grafoSalvo, sizeof(Grafo), 1, arquivo);
-    
+
     // Fechar o arquivo
     fclose(arquivo);
 }
@@ -680,5 +709,3 @@ void adicionarNoAdjacente(ListaNoAdjacente* lista, NoAdjacente* noAdjacente){
     }
     lista->tam++;
 }
-
-

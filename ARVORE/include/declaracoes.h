800483592bc0fc0cb8736b0c901bd131749ab512
Adalberto dos Santos Pinheiro <117361164+Adalbert-s@users.noreply.github.com>
2023-06-24 23:47:28 -0300

    Update declaracoes.h

--- ARVORE/include/declaracoes.h
+++ ARVORE/include/declaracoes.h
@@ -3,38 +3,40 @@
 #ifndef DECLARACOES_H
 #define DECLARACOES_H
 
-//funcoes de manipulacao de arquivos
-// funcoes para escrever e ler os dados dos integrantes em arquivos
+//funcoes responaveis por criar pastas
+short int   cria_pastas         (void);                     //essa funcao e responsavel por criar as pastas
+short int   verificaPasta       (const char* nomePasta);    //essa funcao verifica se a pasta existe ou não e retorna um valor
 
-short int   cria_pastas         (void);                     //funcao para a criacao de pastas, tipo short int pois o short int ja � mais que o necessario para essa funcao.
-short int   verificaPasta       (const char* nomePasta);
+//funcoes de manipulacao de informacoes
 
-//funcoes de manipulacao
-
-int         salvar              (ListaLDE *inicio, ListaLDE *aux, TCabecalho cab);
-void        cadastrar           ();
-void        recuperar           ();
+int         salvar              (ListaLDE *inicio, ListaLDE *aux, TCabecalho cab);  //funcao responsavel por salvar a lista em arquivo
+void        cadastrar           ();                                                 //funcao responsavel por cadrastrar pessoas
+void        recuperar           ();                                                 //funcao responsavel por recuperar as informacoes do arquivo
 
 //funcoes complementares.
 
-void        Adiciona_filho      (casal *C);
-void        Adiciona_conjugue   (casal *C);
-short int   Adiciona_par        (casal *C);
-void        Adiciona_filho      (casal *C);
+void        Adiciona_filho      (casal *C);                 //funcao responsavel por adicionar informacoes do filho
+void        Adiciona_conjugue   (casal *C);                 //funcao responsavel por adicionar informacoes do conjugue
+short int   Adiciona_par        (casal *C);                 //funcao que completa os campos com desconhecido se for o caso
+void        PreencheFicha              (void *struct_ptr, int tipo);    //funcao responsavel por capturar informacoes da ficha medica
+void        PreencheCaracteristicas    (void *struct_ptr, int tipo);    //funcao responsavel por preencher as informacoes de caracteristica fisica
+              
+//funcoes responsaveis por captura de buffer
 
-int         obterResposta       (const char* resposta);
+int         obterResposta       (const char* resposta);     //funcao que captura o buffer
+void        limparBuffer        ();                         //funcao de limpar o buffer
+char*       lerResposta         (int tamanho, FILE* fluxo); //funcao que le a resposta digitada
 
-void PreencheFicha              (void *struct_ptr, int tipo);
-void PreencheCaracteristicas    (void *struct_ptr, int tipo);
+//funcoes de ID
 
-void limparBuffer();
-char* lerResposta(int tamanho, FILE* fluxo);
+void gerarID(casal *C);                                     //funcao que gera id
+void salvarID(int idPadrao);                                //funcao que salva o id em arquivo
+int lerID();                                                //funcao auxiliar 
 
-void gerarID(casal *C);
-void salvarID(int idPadrao);
-int lerID();
+void ancestral(ListaLDE *aux, casal *C);                    //funcao responsavel por dar mensagens 
 
-void ancestral(ListaLDE *aux, casal *C);
-void usage(int menu);
+//funcao de menu
+
+void usage(int menu);                                       //responsavel por mostrar legendas para auxiliar o usuario
 
 #endif // DECLARACOES_H
